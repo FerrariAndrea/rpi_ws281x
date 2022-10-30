8390895d980b9623d0d79dc424d2e44eb77e5ed1
Andrea Ferrari <33545798+FerrariAndrea@users.noreply.github.com>
2022-10-30 19:38:20 +0100

    fix stdout

--- ws2811.c
+++ ws2811.c
@@ -701,7 +701,7 @@ static int check_hwver_and_gpionum(ws2811_t *ws2811)
     rpi_hw = ws2811->rpi_hw;
     hwver = rpi_hw->hwver & 0x0000ffff;
     gpionum = ws2811->channel[0].gpionum;
-    fprintf(stdout, "[DEBUG]hwver: \n", hwver);
+    fprintf(stdout, "[DEBUG]hwver: %d\n", hwver);
     if(hwver < 0x0001){//Banana Pi M2 Zero
         return set_driver_mode(ws2811, gpionum);
     }else if (hwver < 0x0004)  // Model B Rev 1
